c6c943f738303ad127e9a2156b5268c2ce979a0e
Dragan Pepic <d.pepic@hotmail.com>
2021-08-25 18:27:28 +0200

    Add project files.

---
+++ Staticnost/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Staticnost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba.Ime = "Pera";
+
+           while(true)
+           {
+                Osoba a = new ();
+                Osoba.Brojac++;
+                Console.WriteLine($"Broj osoba je {Osoba.Brojac}");
+                Console.ReadKey();
+
+           }
+        }
+    }
+
+    class Osoba
+    {
+        public string Ime = "asd";
+        public static int Brojac;
+
+        public static void Nesto()
+        {
+            
+            Console.WriteLine("asd");
+        }
+    }
+
+}
